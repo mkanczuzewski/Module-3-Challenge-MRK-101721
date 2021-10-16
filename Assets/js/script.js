916c4fe782be8374b6bfeb777d3a31528c9dce2e
mkanczuzewski <mkanczuzewski@gmail.com>
2021-10-16 15:31:06 -0400

    Add list of allowed characters

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,3 +1,9 @@
+// create a list of allowed characters
+var charLowerAlpha = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]
+var charUpperAlpha = [A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z]
+var charNumber = [1,2,3,4,5,6,7,8,9,0]
+var charSpecial = ['!','@','#','%','^','&','*','?','%','~']
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");{
   //ask for length of password between 8 and 128 characters
