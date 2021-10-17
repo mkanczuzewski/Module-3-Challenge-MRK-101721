9b79111e24ea7775038b90afe0d133fe39dcd543
mkanczuzewski <mkanczuzewski@gmail.com>
2021-10-16 20:51:59 -0400

    Add in numeric and special characters

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -7,7 +7,7 @@
 var charLowerAlpha = ["a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"];
 var charUpperAlpha = ["A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z"];
 var charNumber = [1,2,3,4,5,6,7,8,9,0];
-var charSpecial = ['!','@','#','%','^','&','*','?','%','~'];
+var charSpecial = ['!','@','#','%','^','&','*','?','%','~','(',')'];
 var passwordChar = ""
 
 var generateBtn = document.querySelector("#generate");
@@ -28,7 +28,7 @@ var generatePassword = function() {
     if (isNaN(promptLength)) {
     window.alert("You did not pick a valid option. Try again.");
     }
-    else if (promptLength < 8 && promptLength > 128) {
+    else if (promptLength < 8 || promptLength > 128) {
     window.alert("You did not pick a valid option. Try again.");
     }
     else{
@@ -41,19 +41,32 @@ var generatePassword = function() {
   //prompt if they want lower case alpha characters
   var promptLowerAlpha = window.confirm("Do you want lower case characters?")
   if (promptLowerAlpha) {
-  passwordChar += charLowerAlpha;
-  passwordChar += ','
-  console.log(passwordChar);
-  console.log(charLowerAlpha);
+    passwordChar += charLowerAlpha;
+    passwordChar += ','
+    console.log(passwordChar);
   }
 
   //ask if user wants upper case letters
   var promptUpperAlpha = window.confirm("Do you want upper case characters?")
   if (promptUpperAlpha) {
-  passwordChar += charUpperAlpha;
-  passwordChar += ','
-  console.log(passwordChar);
-  console.log(charUpperAlpha);
+    passwordChar += charUpperAlpha;
+    passwordChar += ','
+    console.log(passwordChar);
+  }
+
+  //ask if user wants numeric characters
+  var promptNumeric = window.confirm("Do you want numeric characters?")
+  if (promptNumeric) {
+    passwordChar += charNumber
+    passwordChar += ','
+    console.log(passwordChar);
+  }
+
+  //ask if user wants special characters
+  var promptSpecial = window.confirm("Do you want special characters?")
+  if (promptSpecial) {
+    passwordChar += charSpecial
+    console.log(passwordChar);
   }
 }
 
